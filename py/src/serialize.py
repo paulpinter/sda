035a67b0ba13fbaaa6cdc99a59d46f4ce96701dd
Paul Pinter <paul.pinter@fastmail.com>
2024-06-23 23:49:53 +0200

    add serilalizer

--- py/src/serialize.py
+++ py/src/serialize.py
@@ -29,3 +29,12 @@ def ordering_to_space_separated_string(ordering: list[int]) -> str:
     Convert an ordering to a space-separated string.
     """
     return " ".join(map(str, ordering))
+
+
+def store_in_file(path: str, dump: list[str]) -> None:
+    """
+    Store the dump in a file.
+    """
+    with open(path, "w") as f:
+        for line in dump:
+            f.write(f"{line}\n")
